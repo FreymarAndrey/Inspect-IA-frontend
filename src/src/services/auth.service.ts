be53b0ebb7f4831d3bfacf37e13a3fd07cbd3fb8
Freymar Sanchez <freymar.sanchez@wiedii.co>
2025-09-16 10:27:06 -0500

    test environment login

--- src/src/services/auth.service.ts
+++ src/src/services/auth.service.ts
@@ -3,7 +3,7 @@ import { AuthEndpoints } from "src/models";
 import { loadAbort } from "src/utilities";
 
 export class AuthService {
-  private static API_URL = `${import.meta.env.VITE_API_URL}/auth`;
+  private static API_URL = `https://app-bc7f91f1-a99f-4712-b2a3-481e0a1772ac.cleverapps.io/api/v1/auth`;
 
   static register(data: any) {
     const controller = loadAbort();
@@ -15,6 +15,8 @@ export class AuthService {
 
   static login(email: string, password: string) {
     const controller = loadAbort();
+    console.log(import.meta.env);
+    console.log(import.meta.env.VITE_API_URL);
     return {
       call: axios.post(`${this.API_URL}/${AuthEndpoints.login}`, {
         email,
